c76d59ba7f78f8a66d09663e458c938011955f04
alex <akorotkov@mcfr.ru>
2015-09-03 20:03:39 +0600

    services

--- hydra.py
+++ hydra.py
@@ -5,35 +5,44 @@ import os
 
 application = Flask(__name__)
 
-def get_items_list():
-    ENABLED = os.listdir(application.config["ENABLED"])
-    AVAILABLE = os.listdir(application.config["AVAILABLE"])
+SERVICES = {
+    "nginx": {
+        "ENABLED": "/etc/nginx/sites-available",
+        "AVAILABLE": "/etc/nginx/sites-enabled",
+        "RESTART": "service nginx restart"
+    }
+}
+
+def get_items_list(config):
+    ENABLED = os.listdir(config["ENABLED"])
+    AVAILABLE = os.listdir(config["AVAILABLE"])
     return [(item, item in ENABLED) for item in AVAILABLE]
 
-def enable(item):
-    os.symlink(os.path.join(application.config["AVAILABLE"], item), os.path.join(application.config["ENABLED"], item))
+def enable(config, item):
+    os.symlink(os.path.join(config["AVAILABLE"], item), os.path.join(config["ENABLED"], item))
 
-def disable(item):
+def disable(config, item):
     os.unlink(os.path.join(application.config["ENABLED"], item))
 
-@application.route("/")
-@application.route("/items/")
-def items_list():
-    return render_template("items_list.html", vhosts=get_items_list())
+@application.route("/<service>/")
+def items_list(service):
+    return render_template("items_list.html", vhosts=get_items_list(SERVICES[service]), service=service)
 
-@application.route("/items/save/", methods=["post"])
-def save_items_status():
-     ACTIVE = request.form.getlist("items")
-     ENABLED = os.listdir(application.config["ENABLED"])
-     AVAILABLE = os.listdir(application.config["AVAILABLE"])
-     for item in AVAILABLE:
-          if item in ACTIVE:
-              if item not in ENABLED:
-                  enable(item)
-          else:
-              if item in ENABLED:
-                  disable(item)
-     return redirect(url_for("items_list"))
+@application.route("/<service>/save/", methods=["post"])
+def save_items_status(service):
+    config = SERVICES[service]
+    ACTIVE = request.form.getlist("items")
+    ENABLED = os.listdir(config["ENABLED"])
+    AVAILABLE = os.listdir(config["AVAILABLE"])
+    for item in AVAILABLE:
+         if item in ACTIVE:
+             if item not in ENABLED:
+                 enable(config, item)
+         else:
+             if item in ENABLED:
+                 disable(config, item)
+    return redirect(url_for("items_list", service=service))
      
 if __name__ == "__main__":
-     application.run()
+    application.run()
+    
