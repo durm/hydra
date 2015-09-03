c3c4c50119f6756f5440ecffd903a0cf8031e13b
alex <akorotkov@mcfr.ru>
2015-09-03 18:47:44 +0600

    itema

--- hydra.py
+++ hydra.py
@@ -1,41 +1,39 @@
+# -*- coding: utf-8 -*-
 
 from flask import Flask, render_template, request, url_for, redirect
 import os
 
 application = Flask(__name__)
 
-SITES_AVAILABLE = "/etc/nginx/sites-available"
-SITES_ENABLED = "/etc/nginx/sites-enabled"
+def get_items_list():
+    ENABLED = os.listdir(application.config["ENABLED"])
+    AVAILABLE = os.listdir(application.config["AVAILABLE"])
+    return [(item, item in ENABLED) for item in AVAILABLE]
 
-def get_vhosts_list():
-    result = []
-    sites_enabled = os.listdir(SITES_ENABLED)
-    for f in os.listdir(SITES_AVAILABLE):
-        result.append((f, f in sites_enabled))
-    return result
+def enable(item):
+    os.symlink(os.path.join(application.config["AVAILABLE"], item), os.path.join(application.config["ENABLED"], item))
 
-def enable_site(site):
-    os.symlink(os.path.join(SITES_AVAILABLE, site), os.path.join(SITES_ENABLED, site))
-
-def disable_site(site):
-    os.unlink(os.path.join(SITES_ENABLED, site))
+def disable(item):
+    os.unlink(os.path.join(application.config["ENABLED"], item))
 
 @application.route("/")
-def vhosts():
-    return render_template("vhosts.html", vhosts=get_vhosts_list())
+@application.route("/items/")
+def items_list():
+    return render_template("items_list.html", vhosts=get_items_list())
 
-@application.route("/hosts/save/", methods=["post"])
-def save_vhosts_status():
-     active_vhosts = request.form.getlist("vhost")
-     sites_enabled = os.listdir(SITES_ENABLED)
-     for f in os.listdir(SITES_AVAILABLE):
-          if f in active_vhosts:
-              if f not in sites_enabled:
-                  enable_site(f)
+@application.route("/items/save/", methods=["post"])
+def save_items_status():
+     ACTIVE = request.form.getlist("items")
+     ENABLED = os.listdir(application.config["ENABLED"])
+     AVAILABLE = os.listdir(application.config["AVAILABLE"])
+     for item in AVAILABLE:
+          if item in ACTIVE:
+              if item not in ENABLED:
+                  enable(item)
           else:
-              if f in sites_enabled:
-                  disable_site(f)
-     return redirect(url_for("vhosts"))
+              if item in ENABLED:
+                  disable(item)
+     return redirect(url_for("items_list"))
      
 if __name__ == "__main__":
      application.run()
